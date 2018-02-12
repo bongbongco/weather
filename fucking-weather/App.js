86c69c4d5e64265b3b3268e6e8007de5124148dd
seungyonglee <bongbongco@gmail.com>
2018-02-12 12:27:08 +0900

    First Commit

---
+++ fucking-weather/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    flexDirection: 'row',
+    flexWrap:'wrap'
+  },
+  redView:{
+    height: 50,
+    width: 50,
+    backgroundColor: 'red',
+  },
+  yellowView:{
+    height: 50,
+    width: 50,
+    backgroundColor: 'yellow'
+  }
+});
