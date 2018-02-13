ed7bf858c216c18885274809d28c1d735a393e8e
seungyonglee <bongbongco@gmail.com>
2018-02-13 12:19:14 +0900

    리액트 네이티브 날씨 앱 완성

--- fucking-weather/App.js
+++ fucking-weather/App.js
@@ -1,30 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
+import React, {Component } from 'react';
+import { StyleSheet, Text, View, Image, ActivityIndicator, StatusBar } from 'react-native';
+import Weather from "./Weather";
+
+const API_KEY = "4baa64b959962df1c171b7fb2d7ac5c1";
 
 export default class App extends React.Component {
+  state = {
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
+  }
+
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(
+      position => { 
+          this._getWeather(position.coords.latitude, position.coords.longitude)
+      },
+      error => {
+        this.setState({
+          error: error
+        });
+      }
+    );
+  }
+
+  _getWeather= (lat, long) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`
+    )
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      this.setState({
+        temperature:json.main.temp,
+        name:json.weather[0].main,
+        isLoaded: true
+      })
+    })
+  }
+
   render() {
+    const { isLoaded, error, temperature, name } = this.state;
     return (
       <View style={styles.container}>
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
+      <StatusBar hidden={true} />
+        {isLoaded ? (
+          <Weather 
+            weatherName={name} 
+            temp={Math.ceil(temperature - 273.15)}
+          />) : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>
+              Getting the fucking weather
+            </Text>
+          {error ? (
+            <Text style={styles.errorText}>
+              {error}
+            </Text>) : null}
+          </View>
+        )}
       </View>
     );
   }
@@ -33,20 +68,21 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
-    flexDirection: 'row',
-    flexWrap:'wrap'
+    backgroundColor: '#fff'
   },
-  redView:{
-    height: 50,
-    width: 50,
-    backgroundColor: 'red',
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
   },
-  yellowView:{
-    height: 50,
-    width: 50,
-    backgroundColor: 'yellow'
+  loading:{
+    flex:1,
+    backgroundColor:'#FDF6AA',
+    justifyContent: 'flex-end',
+    paddingLeft: 25
+  },
+  loadingText:{
+    fontSize: 38,
+    marginBottom: 100
   }
 });
